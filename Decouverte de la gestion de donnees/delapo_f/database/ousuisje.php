05b2fdd49e6a47edc710d1e00b9ca3db3170f05a
meniga_l <meniga_l@1586576b-5c7f-497b-855c-82c868351710>
2015-02-05 18:24:01 +0000

    big backup

---
+++ Decouverte de la gestion de donnees/delapo_f/database/ousuisje.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Titre</title>
+    </head>
+<body>
+<iframe
+  width="400"
+  height="400"
+  frameborder="0" style="border:0"
+  src="https://www.google.com/maps/embed/v1/place?key=AIzaSyB0anrCFT6RpF4EXDYidwQJWx2vhd7DcnI                            &q=Space+Needle,Seattle+WA">
+</iframe>
+</body>
+</html>
+
